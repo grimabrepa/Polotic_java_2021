87d04dcf2e5cde5b63041e6a0f011293c353e5f8
Marcelo Peralta <ezcajo@yahoo.com.ar>
2021-03-28 02:06:00 -0300

    Primera subida

---
+++ actividad01/actividad01/src/actividad01/Actividad01.java
@@ -0,0 +1,27 @@
+
+package actividad01;
+/** 
+ * @author Marcelo
+ * actividad: 1) Probar realizar las operaciones que se citan a continuación en el IDE Netbeans, registrar los errores obtenidos y responder a las preguntas citadas.
+a. Declarar una variable de tipo entero, otra de tipo double y otra de tipo String.
+b. Asignar un valor a la variable de tipo entero y asignar esta última a la variable de tipo double.
+* ¿Existe un error? ¿Se puede hacer esta operación?
+c. Asignar un valor a la variable de tipo double y asignar esta última a la variable de tipo entero. 
+* ¿Existe un error? ¿Se puede hacer esta operación?
+d. Asignar el valor 35 a la variable de tipo String. ¿Existe un error? ¿Se puede hacer esta operación?
+e. Si quiero almacenar el número 48 en la variable String. ¿Cómo debo realizar la asignación?
+2) Realizar un programa que permita el intercambio de valores entre dos variables. 
+* Por ejemplo: Si una variable numero vale 35, y una variable numero2 vale 20, realizar las acciones necesarias para que numero pase a valer 20 y numero2 pase a valer 35. Una vez realizado el cambio mostrar el resultado por pantalla.
+ */
+public class Actividad01 {
+
+    public static void main(String[] args) {
+  //Item a:
+  int entero;
+  double doble;
+  String cadena;
+  //b
+  
+    }
+    
+}
