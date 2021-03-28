f080b71ae2759e64fabaad2464a164e287240eae
Marcelo Peralta <ezcajo@yahoo.com.ar>
2021-03-28 02:36:22 -0300

    Actividad 01 - finalizada

--- actividad01/actividad01/src/actividad01/Actividad01.java
+++ actividad01/actividad01/src/actividad01/Actividad01.java
@@ -11,17 +11,49 @@ c. Asignar un valor a la variable de tipo double y asignar esta última a la var
 d. Asignar el valor 35 a la variable de tipo String. ¿Existe un error? ¿Se puede hacer esta operación?
 e. Si quiero almacenar el número 48 en la variable String. ¿Cómo debo realizar la asignación?
 2) Realizar un programa que permita el intercambio de valores entre dos variables. 
-* Por ejemplo: Si una variable numero vale 35, y una variable numero2 vale 20, realizar las acciones necesarias para que numero pase a valer 20 y numero2 pase a valer 35. Una vez realizado el cambio mostrar el resultado por pantalla.
+* Por ejemplo: Si una variable numero vale 35, y una variable numero2 vale 20, 
+* realizar las acciones necesarias para que numero pase a valer 20 y numero2 pase a valer 35. 
+* Una vez realizado el cambio mostrar el resultado por pantalla.
  */
 public class Actividad01 {
 
     public static void main(String[] args) {
-  //Item a:
+  //ACTIVIDAD 1.1
+    //Item a:
   int entero;
   double doble;
   String cadena;
-  //b
   
+  //b: respuesta: No se produjo ningun error, esta operacion es valida por que la variable receptora
+  // es de mayor capacidad que la variable contenida
+  entero = 2021;
+  doble = entero;
+
+  //c: Hay error en este tipo de asignaciones- esta operacion presenta error de tipo, esta operacion
+  // se soluciona realizando un casteo para cambiar el tipo de la caja contenida
+  doble = 20.21;
+  entero = (int)doble;
+  
+  //d: No hay error por que el valor 35 fue introduciod como una cadena al colocarle la doble comillas
+  // esta operacion es valida.
+  cadena = "35";
+  
+  //e: En el punto d, se explica la operacion a realizar
+  cadena = "48";
+  
+  //ACTIVIDAD 1.2
+    int numero, numero2, aux;
+    numero =35;
+    numero2 = 20;
+        System.out.println("Numeros iniciales");
+        System.out.println("El valor 1 es: " + numero);
+        System.out.println("El valor 2 es: " + numero2);
+    aux= numero;
+    numero=numero2;
+    numero2=aux;
+        System.out.println("\nNumeros intercambiados");
+        System.out.println("El valor 1 es: " + numero);
+        System.out.println("El valor 2 es: " + numero2);
     }
-    
-}
+  }
+
